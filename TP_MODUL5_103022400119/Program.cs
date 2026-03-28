027c8872a9a43e45925bbdfda8be0c8856de5b81
Daniarthawiku <wikunugroho735@gmail.com>
2026-03-28 22:17:09 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TP_MODUL5_103022400119/Program.cs
+++ TP_MODUL5_103022400119/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T x)
+    {
+        Console.WriteLine($"Halo user {x}");
+    }
+
+    static void Main(string[] args)
+    {
+        HaloGeneric hGen = new HaloGeneric();
+        string namaUser = "dani";
+
+       hGen.SapaUser<string>(namaUser);
+    }
+}
--- TP_MODUL5_103022400119/obj/Debug/net8.0/TP_MODUL5_103022400119.AssemblyInfo.cs
+++ TP_MODUL5_103022400119/obj/Debug/net8.0/TP_MODUL5_103022400119.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL5_103022400119")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371c3cf5853a764cbca763db6ace2f36074fa225")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL5_103022400119")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL5_103022400119")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
