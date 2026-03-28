344e9e794c94e9d7f47be32a050e4fad0dc3ec63
Daniarthawiku <wikunugroho735@gmail.com>
2026-03-28 22:37:54 +0700

    menambahkan class DataGeneric

--- TP_MODUL5_103022400119/Program.cs
+++ TP_MODUL5_103022400119/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ModulGeneric
+{
+    public class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"nim dani adalah {this.data}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<string> objekData = new DataGeneric<string>("103022400119");
+            objekData.PrintData();
+        }
+    }
+}
